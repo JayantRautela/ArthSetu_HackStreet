bc0ceeac3e8de104085d76d5a4c71599ca413425
JayantRautela <jayantrautela0205@gmail.com>
2026-02-26 19:15:01 +0530

    update links

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL
+  origin: 'https://arth-setu-ten.vercel.app/'
 }));
 
 const PORT = process.env.PORT;
